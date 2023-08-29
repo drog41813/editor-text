ca2c2ef900815969eaa4a4e5259c69ce094d2b5a
drog41813 <daniel.lee042003@gmail.com>
2023-08-28 19:36:45 -0500

    added plugin and asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const {  warmStrategyCache } = require('workbox-recipes');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+
+const assetCache = new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
+    }),
+  ],
+}); 
+
+registerRoute(({ request}) => request.mode === 'navigate', assetCache)
 registerRoute();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add CSS loaders and babel to webpack
 
 module.exports = () => {
   return {
@@ -19,12 +19,52 @@ module.exports = () => {
     },
     plugins: [
       
-    ],
-
-    module: {
-      rules: [
-        
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          chunks: ['main'],
+        }),
+        new HtmlWebpackPlugin({
+          template: './src/install.html',
+          filename: 'install.html',
+          chunks: ['install'],
+        }),
+        new WebpackPwaManifest({
+          name: 'Your App Name',
+          short_name: 'App',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#000000',
+          icons: [
+            {
+              src: path.resolve('src/assets/icon.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+            },
+          ],
+        }),
+        new InjectManifest({
+          swSrc: './src/sw.js',
+        }),
       ],
-    },
+    
+
+      module: {
+        rules: [
+          {
+            test: /.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+        ],
+      },
+    };
   };
-};
